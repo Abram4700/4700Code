2db25c436587f52ee7ad3443d791ce3e2bba579b
Abram4700 <abrammazal@cmail.carleton.ca>
2017-04-14 15:36:17 -0400

    Minor changes
    Does not pre-allocate looped matrix the right way

--- MCTraj/MCtraj.m
+++ MCTraj/MCtraj.m
@@ -1,27 +1,31 @@
-% clear all
+
+% Clear all global/local variables and close all figures
 clearvars
 clearvars -GLOBAL
 close all
 % set(0,'DefaultFigureWindowStyle','docked')
-global C
+global C % Declare global variable
 
+% Add new path to workspace/search path
 addpath ../geom2d/geom2d
 
-C.q_0 = 1.60217653e-19;             % electron charge
-C.hb = 1.054571596e-34;             % Dirac constant
-C.h = C.hb * 2 * pi;                    % Planck constant
-C.m_0 = 9.10938215e-31;             % electron mass
-C.kb = 1.3806504e-23;               % Boltzmann constant
-C.eps_0 = 8.854187817e-12;          % vacuum permittivity
-C.mu_0 = 1.2566370614e-6;           % vacuum permeability
-C.c = 299792458;                    % speed of light
-C.g = 9.80665 %metres (32.1740 ft) per s²
+% Declare constants
+C.q_0   = 1.60217653e-19;   % Electron Charge
+C.hb    = 1.054571596e-34;  % Dirac Constant
+C.h     = C.hb*2*pi;        % Planck Constant
+C.m_0   = 9.10938215e-31;   % Electron Mass
+C.kb    = 1.3806504e-23;    % Boltzmann Constant
+C.eps_0 = 8.854187817e-12;  % Vacuum Permittivity
+C.mu_0  = 1.2566370614e-6;  % Vacuum Permeability
+C.c     = 299792458;        % Speed of Light
+C.g     = 9.80665;          % Metres (32.1740 ft) per s²
 
 nTime = 100;
 nTraj = 10;
 nSims = 100;
 
-InitalAngle = 55 * pi / 180;
+Angle = 45; % Set initial angle (degrees)
+InitalAngle = Angle * pi / 180;
 PlotTraj = 1;
 MaxC = 10000;
 doPlot = 1;
@@ -32,7 +36,7 @@ c = 2;
 dt = 1;
 
 Wind = @UniformRandWind;
-WindParas = [10];
+WindParas = (10);
 
 % Wind = @NormalRandWind;
 % WindParas = [5];
@@ -41,7 +45,7 @@ WindParas = [10];
 % WindParas = [.35];
 
 
-xl = zeros(1,1000); % Pre-allocate matrix for increased performance
+xl = zeros(1,1000); 
 
 for n = 1: nSims
     x(1, :) = zeros(1, nTraj);
